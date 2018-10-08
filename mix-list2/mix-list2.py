fe798a038422b6b07d090541a9ce48c2d9c9eb8a
mullimic <mullinsm00@gmail.com>
2018-10-08 17:44:49 -0400

    updated lisst info

--- mix-list2/mix-list2.py
+++ mix-list2/mix-list2.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 proto = ['ssh', 'http', 'https']
+protoa = ['ssh', 'http', 'https']
 print(proto)
-print(proto[1])
 proto.append('dns') # this line will add 'dns' to the end of our list
+protoa.append('dns')
 print(proto)
+proto2 = [ 22, 80, 443, 53 ]
+proto.extend(proto2)
+print(proto)
+protoa.append(proto2)
+print(protoa)
