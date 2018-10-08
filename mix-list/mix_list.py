08675d483656c0c2be6ba9837908e615029d4f26
mullimic <mullinsm00@gmail.com>
2018-10-08 17:37:46 -0400

    fixed final list check

--- mix-list/mix_list.py
+++ mix-list/mix_list.py
@@ -14,6 +14,12 @@ print("The last item in the list (state): " + my_list[2] )
 
 new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
-#print("When I" + str(new_list[5]) "into IP addresses" + str(new_list[3]) "or" + str(new_list[4]) "I am unable to ping ports" + str(new_list[0]) + str(new_list[1]) + str(new_list[2] ) )
 
+port1 = new_list[0]
+port2 = new_list[1]
+port3 = new_list[2]
+ip1 = new_list[3]
+ip2 = new_list[4]
+conn = new_list[5]
 
+print("When I " + conn + " into IP addresses " + ip1 + " or " + ip2 + " I am unable to ping ports " + str(port1) + " , " + port2 + " or " + str(port3) )
