7b41d792230b3ce51c095e7fda60bf69e4daab8c
mullimic <mullinsm00@gmail.com>
2018-10-08 15:34:44 -0400

    New test input

---
+++ lab_input/input_ip.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+user_input = input('Please enter an IPv4 IP address:')
+print("you told me the UIPv4 address is:" + user_input)
+user_input = input('Please enter the vendor name:')
+print("vendor name is:" + user_input)
