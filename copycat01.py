c52052877919af5e625a3e0aaea043352bdfdc23
mullimic <mullinsm00@gmail.com>
2018-10-08 16:02:46 -0400

    Updated comments

--- copycat01.py
+++ copycat01.py
@@ -3,9 +3,12 @@
 import shutil
 import os
 
+#force python to start in this directory
 os.chdir('/home/student/mycode/')
 
+#Cory file to a new file
 shutil.copy('5g_research/sdn_network.txt', '5g_research/sdn_network.txt.copy')
 
+#Copy this while directory to another location
 shutil.copytree('5g_research/', '5g_research_backup/')
 
