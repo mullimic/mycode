1e3d2b795e2fbadff64bee58439fc2eae62724d2
mullimic <mullinsm00@gmail.com>
2018-10-09 16:53:37 -0400

    new code

---
+++ my2to3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+thisround = 0
+while(True):
+   print('What is the IPv4 address used to broadcast on a local netowrk? ')
+   answer = input()
+   thisround = thisround + 1
+   if (answer == '255.255.255.255'):
+      print('Correct!')
+      break
+   elif (thisround ==3):
+      print('Sorry, the answer was 255.255.255.255')
+      break
+   else:
+      print('Sorry. Try again!')
